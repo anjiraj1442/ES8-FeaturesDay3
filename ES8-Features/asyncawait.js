3038b1bbe70b6ed313c886595f84676a60ddc180
anjiraj1442 <anjiraj1442@gmail.com>
2021-12-16 08:50:02 +0530

    Added:async and await examle is added

---
+++ ES8-Features/asyncawait.js
@@ -0,0 +1,36 @@
+//asyn and await with example
+
+
+function reacharge() {
+     return new Promise(resolve => {
+       setTimeout(() => {
+         resolve('enter mobile number');
+       }, 2000);
+     });
+   }
+   
+   function number() {
+     return new Promise(resolve => {
+       setTimeout(() => {
+         resolve('select the plan');
+       }, 3000);
+     });
+   }
+   
+   function plan() {
+     return new Promise(resolve => {
+       setTimeout(() => {
+         resolve('recharge succesfull');
+       }, 2000);
+     });
+   }
+   
+   async function test() {
+     const re = await reacharge()
+     const nu = await number()
+     const pl = await plan()
+   
+     console.log(`${ re } ${ nu } ${ pl }`);
+   }
+   
+   test();
